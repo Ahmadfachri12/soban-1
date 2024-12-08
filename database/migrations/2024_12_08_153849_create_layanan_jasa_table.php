8ebcc14f0d79342453568311284dfa17fd621aec
Marsela0603 <marselasela3554@gmail.com>
2024-12-08 23:21:46 +0700

    membuat tabel di database

---
+++ database/migrations/2024_12_08_153849_create_layanan_jasa_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('layanan_jasa', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('penyedia_jasa_id')->constrained('penyedia_jasa')->onDelete('cascade');
+            $table->string('namaJasa');
+            $table->foreignId('kategori_id')->constrained('kategori')->onDelete('cascade');
+            $table->string('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('layanan_jasa');
+    }
+};
---
+++ database/migrations/2024_12_08_161116_create_pesanan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('layanan_jasa_id')->constrained('layanan_jasa')->onDelete('cascade');
+            $table->date('waktuPemesanan');
+            $table->enum('statusPembayaran', ['Belum dibayar', 'Sudah dibayar'])->default('Belum dibayar');
+            $table->float('totalHarga');
+            $table->enum('statusPesanan', ['Belum diproses', 'Sedang diproses', 'Selesai'])->default('Belum diproses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanan');
+    }
+};
