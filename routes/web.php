d0cee944e2147f6be3c0bc6429eb97aa838ce5d8
eriksetiawan01 <149347297+eriksetiawan01@users.noreply.github.com>
2024-12-10 12:26:16 +0700

    Menambahkan dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [AdminController::class, 'index'], function () {
+    return view('admin.index');
+} );
 
 Route::get('/', function () {
     return view('user.index'); // Nama file blade
